7f609b27a9a41965321dfff3ec7353b5d3cbc8b7
Brian Lauber <constructible.truth@gmail.com>
2012-06-03 00:22:47 -0400

    Initial commit.  This is just an empty rails application.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RubyVault::Application.config.secret_token = 'd42006b715881e81354eeee6e40ff419b8c055497d11ecd4910e9ba7dbdc1be4ba46b326936b490c81d3ef179a12a125c9d5b82035216e7418c499dfcd2f912f'
